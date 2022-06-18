a3b316791e25bfb75c9c6289a05bb4150a353fa8
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-06-18 07:37:04 -0700

    hotfix ABC-234

--- backEndApp.py
+++ backEndApp.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+beapp = Flask(__name__)
 
-@app.route('/')
+@beapp.route('/')
 def hello_world():
     return 'myBackendService 0.0.1 says Hello, Docker!'
